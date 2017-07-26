31a0717958739f534dd5b17bbaa973c15183848a
Jeremy Michaud <jeremy.michaud@heig-vd.ch>
2017-07-26 16:16:52 +0200

    changed javadoc

--- doc/search/all_c.js
+++ doc/search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['nrf52_20software_20',['nRF52 software ',['../md__d_1_n_r_f5__s_d_k_12_82_80_f012efa_examples_ble_peripheral_ble_app_template__projet__r_e_a_d_m_e.html',1,'']]],
+  ['nrf52_20software',['nRF52 software',['../md__d_1_n_r_f5__s_d_k_12_82_80_f012efa_examples_ble_peripheral_ble_app_template__projet__r_e_a_d_m_e.html',1,'']]],
   ['name_5fchar_5fadd',['name_char_add',['../group__ble__admin__service.html#gab88b9e17d9db22c4f4ad7fc3fe827ae5',1,'admin_service.c']]],
   ['name_5fwrite_5fhandler',['name_write_handler',['../structble__adm__s.html#ae480cc0e81b69890df0cb9a5436bef6f',1,'ble_adm_s::name_write_handler()'],['../group__ble__sdk__app__main.html#ga391b46aeae9a2b1c7175e6e203136599',1,'name_write_handler():&#160;main.c']]],
   ['next_5fconn_5fparams_5fupdate_5fdelay',['NEXT_CONN_PARAMS_UPDATE_DELAY',['../group__ble__sdk__app__main.html#gadf85796ef07632ed27e0bce9509d9245',1,'main.c']]],
--- doc/search/pages_0.js
+++ doc/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['nrf52_20software_20',['nRF52 software ',['../md__d_1_n_r_f5__s_d_k_12_82_80_f012efa_examples_ble_peripheral_ble_app_template__projet__r_e_a_d_m_e.html',1,'']]]
+  ['nrf52_20software',['nRF52 software',['../md__d_1_n_r_f5__s_d_k_12_82_80_f012efa_examples_ble_peripheral_ble_app_template__projet__r_e_a_d_m_e.html',1,'']]]
 ];
